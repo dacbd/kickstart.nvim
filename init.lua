6389c6b4ef2f1acb6cbea1591c6fa7b11ea830d4
Daniel Barnes <dabarnes2b@gmail.com>
2024-10-22 12:33:34 +0900

    update

--- init.lua
+++ init.lua
@@ -635,7 +635,8 @@ require('lazy').setup({
         rust_analyzer = {},
         terraformls = {},
         tailwindcss = {},
-        tsserver = {},
+        ts_ls = {},
+        grammarly = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
