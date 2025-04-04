1846947a19b071814bd342c1e0326a382dc1b08b
Daniel Barnes <dabarnes2b@gmail.com>
2025-04-04 15:48:48 +0900

    add bashls

--- init.lua
+++ init.lua
@@ -649,6 +649,7 @@ require('lazy').setup({
         ts_ls = {},
         -- yamlls = {},
         helm_ls = {},
+        bashls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
