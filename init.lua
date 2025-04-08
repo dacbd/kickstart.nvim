93329b4b381853596a8884e950f2e96c20f1f2ff
Daniel Barnes <dabarnes2b@gmail.com>
2025-04-09 07:59:12 +0900

    add dockerls

--- init.lua
+++ init.lua
@@ -650,6 +650,7 @@ require('lazy').setup({
         -- yamlls = {},
         helm_ls = {},
         bashls = {},
+        dockerls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
